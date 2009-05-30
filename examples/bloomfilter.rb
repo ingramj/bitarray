00fed18c3f1e5c6cd037a31f375dedec1f1eaccd
Jim Ingram <ingramj@gmail.com>
2009-05-30 17:43:30 -0500

    Changed the hash function in the bloom filter demo

--- examples/bloomfilter.rb
+++ examples/bloomfilter.rb
@@ -1,21 +1,20 @@
+#!/usr/bin/env ruby
 require 'digest/sha1'
-require 'digest/md5'
 require 'bitarray'
 
 # This bloom filter was written as a demonstration of the BitArray class.
 # Therefore, it was written with an eye towards simplicity rather than
 # efficiency or optimization.
 #
-# The size of the filter is configurable, but we always use 10 hashes.
-# 
 # For information about picking parameters for a Bloom Filter, take a look at
 # the Wikipedia page.
 #
 # http://en.wikipedia.org/wiki/Bloom_filter
 #
 class BloomFilter
-  def initialize(m = 1000000)
+  def initialize(m = 1000000, k = 3)
     @size = m
+    @hashes = k < 3 ? 3 : k
     @ba = BitArray.new(@size)
   end
 
@@ -33,17 +32,20 @@ class BloomFilter
   end
 
   private
-  # Return an array of 10 indices to set.
+  # Return an array of @hashes indices to set. 
+  #
+  # We generate as many hash values as needed by using the technique described
+  # by Kirsch and Mitzenmacher[1].
+  #
+  # [1] http://www.eecs.harvard.edu/~kirsch/pubs/bbbf/esa06.pdf
   def hash(input)
-    # We use the SHA1 hash of the input, and divide into five chunks.
-    h = Digest::SHA1.hexdigest(input)
-    ha = [ h[0,8], h[8,8], h[16,8], h[24,8], h[32,8] ]
-    # Do the same with the MD5 hash, divided into four chunks.
-    h = Digest::MD5.hexdigest(input)
-    ha += [ h[0,8], h[8,8], h[16,8], h[24,8] ]
-    # And add in the built-in (murmur) hash.
-    ha.map! {|hi| hi.to_i(16) % @size}
-    ha << input.hash.abs % @size
+    h1 = input.hash.abs % @size
+    h2 = Digest::SHA1.hexdigest(input).to_i(16) % @size
+
+    ha = [h1, h2]
+    1.upto(@hashes - 2) do |i|
+      ha << (h1 + i * h2) % @size
+    end
     return ha
   end
 end
@@ -53,18 +55,18 @@ end
 # look up words. 
 #
 # Using the calculator at http://hur.st/bloomfilter, the optimum number of bits
-# for my system's dictionary file (98569 words), with 10 hashes and a false
-# positive rate of 0.001, is 1,417,185 bits (about 173 Kb).
+# for my system's dictionary file (98569 words), with a false positive rate of
+# 0.001, is 1,417,185 bits (about 173 Kb), and 10 hash functions.
 #
-# Loading the dictionary takes about 7 seconds on my system.
+# Loading the dictionary takes about 4 seconds on my system.
 if __FILE__ == $0
   print "Loading dictionary..."
-  bf = BloomFilter.new(1417185)
+  bf = BloomFilter.new(1417185, 10)
   File.open('/usr/share/dict/words') {|f|
     f.each_line {|w| bf.add(w.chomp)}
   }
   print "done\n"
-  
+
   puts "Enter words to look up, ctrl-d to quit."
   done = false
   while (!done)
